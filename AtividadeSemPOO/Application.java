8cc1c1b9536885b261cbf90f0cd7f02f592bec88
Frouzin <matheusereis44@gmail.com>
2023-03-10 11:00:01 -0300

    aplicação sem POO

---
+++ AtividadeSemPOO/Application.java
@@ -0,0 +1,34 @@
+package AtividadeSemPOO;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Application {
+
+        public static void main (String[] args){
+            Locale.setDefault(Locale.US);
+            Scanner scan = new Scanner(System.in);
+            double xA, xB, xC,yA, yB, yC;
+
+            System.out.println("Enter the measures of triangle X: ");
+            xA = scan.nextDouble();
+            xB = scan.nextDouble();
+            xC = scan.nextDouble();
+
+            System.out.println("Enter the measures of triangle Y: ");
+            yA = scan.nextDouble();
+            yB = scan.nextDouble();
+            yC = scan.nextDouble();
+
+            double p = (xA+xB+xC)/2.0;
+            double areax = Math.sqrt(p*(p-xA)*(p-xB)*(p-xC));
+
+            p = (yA+yB+yC)/2.0;
+            double areay = Math.sqrt(p*(p-yA)*(p-yB)*(p-yC));
+
+            System.out.printf("Triangle X area: %.4f%n", areax);
+            System.out.printf("Triangle Y area: %.4f%n", areay);
+
+            scan.close();
+        }
+    }
---
+++ atividadeCondicaoTernaria.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class atividadeCondicaoTernaria {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um numero");
+
+        String num= 10 >= 0 ? "Positivo" : "Negativo";
+    }
+}
